9b6a57f5762d516e54df42f8e814e5c82bf86002
Becky Robran <rerobran@gmail.com>
2019-12-16 16:02:30 -0700

    adds routes to sessions new and create
    Co-Authored-By: Jomah Fangonilo <jfangonilo@users.noreply.github.com>
    Co-Authored-By: WrenS <wrenisit@users.noreply.github.com>
    Co-Authored-By: Ray <itemniner@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -37,4 +37,7 @@ Rails.application.routes.draw do
   get "/register", to: "users#new"
   post "/register", to: "users#create"
   get "/profile", to: "users#show"
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
 end
