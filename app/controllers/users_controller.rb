f54aaad5961cff08b422096afd374513a60161dc
Becky Robran <rerobran@gmail.com>
2019-12-16 14:59:02 -0700

    Updates private params and adds sad path error message if not saved
    Co-Authored-By: Jomah Fangonilo <jfangonilo@users.noreply.github.com>
    Co-Authored-By: WrenS <wrenisit@users.noreply.github.com>
    Co-Authored-By: Ray <itemniner@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,24 @@ class UsersController<ApplicationController
   end
 
   def create
-    User.create(user_params)
+    new_user = User.new(user_params)
+    if new_user.save
+      flash[:success] = "Congratulations! You are now registered and logged in."
+      session[:user_id] = new_user.id
+      redirect_to "/profile"
+    else
+      flash[:error] = new_user.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(session[:user_id])
   end
 
   private
 
   def user_params
-    params.permit(:name, :address, :city, :state, :zip, :email, :password)
+    params.permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
   end
 end
