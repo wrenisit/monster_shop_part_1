2729cfc464bc76b4e19fba3064fac6a411bbca2a
Wren Steitle <lauren.steitle@gmail.com>
2019-12-16 16:56:39 -0700

    Add routes for log out and redirect to welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -40,4 +40,7 @@ Rails.application.routes.draw do
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
+  get '/logout', to: "sessions#destroy"
+
+  get "/", to: "welcome#index"
 end
