f0f469c3fb4dff5b20ecaef3366b8ba316d0d0ce
Becky Robran <rerobran@gmail.com>
2019-12-16 14:59:14 -0700

    Adds model tests for user
    Co-Authored-By: Jomah Fangonilo <jfangonilo@users.noreply.github.com>
    Co-Authored-By: WrenS <wrenisit@users.noreply.github.com>
    Co-Authored-By: Ray <itemniner@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User <ApplicationRecord
+  validates_presence_of :name, presence: true
+  validates_presence_of :address, presence: true
+  validates_presence_of :city, presence: true
+  validates_presence_of :state, presence: true
+  validates_presence_of :zip, presence: true
+  validates :email, uniqueness: true, case_sensitive: false, presence: true
+  validates_presence_of :password, require: true
+  validates_confirmation_of :password
 
+  has_secure_password
 end
