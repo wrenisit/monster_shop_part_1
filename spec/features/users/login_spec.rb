21db06f0b9c8f7ee38e7e89941725c2ebea1ef1a
Raymond Nguyen <rayknguyen@gmail.com>
2019-12-17 16:58:00 -0700

    Add test for dashboard login
    Co-authored-by: Becky Robran <rerobran@gmail.com>
    Co-authored-by: Jomah Fangonilo <jomah.fangonilo@gmail.com>

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -27,24 +27,6 @@ RSpec.describe "As a visitor when I visit login path", type: :feature do
       expect(page).to have_content("Welcome go@foogle.com")
     end
 
-    it "won't login if password does not match" do
-
-      new_user = create(:regular_user, email: "yo@gmail.com", password: "123password", password_confirmation: "123password")
-
-      visit '/merchants'
-      click_on 'Log In'
-
-      expect(current_path).to eq("/login")
-
-      fill_in :email, with: "yo@gmail.com"
-      fill_in :password, with: "notsecure123"
-      
-      click_button "Log In"
-
-      expect(current_path).to eq("/login")
-      expect(page).to have_content("Sorry Invalid Password or Email.")
-    end
-
     it "won't login if email does not match" do
 
       new_user = create(:regular_user, email: "yo@gmail.com", password: "123password", password_confirmation: "123password")
@@ -63,7 +45,7 @@ RSpec.describe "As a visitor when I visit login path", type: :feature do
       expect(page).to have_content("Sorry Invalid Password or Email.")
     end
 
-    xit "when I am a merchant user I am redirected to my merchant dashboard" do
+    it "when I am a merchant user I am redirected to my merchant dashboard" do
       @diana = create(:merchant_employee)
 
       visit '/merchants'
@@ -77,11 +59,24 @@ RSpec.describe "As a visitor when I visit login path", type: :feature do
       click_button "Log In"
 
       expect(current_path).to eq('/merchants/dashboard')
-
-
+      expect(page).to have_content("Welcome Merchant #{@diana.email}")
     end
 
     xit "when I am an admin user I am redirected to my admin dashboard page" do
+      @barry = create(:admin_user)
+
+      visit '/merchants'
+      click_on 'Log In'
+
+      expect(current_path).to eq("/login")
+
+      fill_in :email, with: @barry.email
+      fill_in :password, with: @barry.password
+     
+      click_button "Log In"
+
+      expect(current_path).to eq('/admin/dashboard')
+      expect(page).to have_content("Welcome Admin #{@barry.email}!")
     end
   end
 end
