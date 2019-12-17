9d0dfb0b5af0e6625aee7aecb05518858e132eea
Becky Robran <rerobran@gmail.com>
2019-12-16 19:26:31 -0700

    Updates create user session for sad path testing
    Co-Authored-By: Jomah Fangonilo <jfangonilo@users.noreply.github.com>
    Co-Authored-By: WrenS <wrenisit@users.noreply.github.com>
    Co-Authored-By: Ray <itemniner@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,13 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    session[:user_id] = user.id
-    flash[:success] = "Welcome #{user.email}"
-    redirect_to "/profile"
+    if user != nil && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome #{user.email}"
+      redirect_to "/profile"
+    else
+      flash[:error] = "Sorry Invalid Password or Email."
+      render:new
+    end
   end
 end
