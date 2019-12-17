b32e0d29755884ba7babed998c1af4c1b645efcb
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-17 15:49:49 -0700

    Add roles spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,38 @@ RSpec.describe User, type: :model do
     it {should validate_uniqueness_of(:email)}
     it {should validate_presence_of(:password)}
   end
+
+  describe "roles" do
+    it "can be created as a user" do
+      user = User.create(name: "penelope",
+                         password: "boom",
+                         role: 0)
+      expect(user.role).to eq("user")
+      expect(user.user?).to be_truthy
+    end
+
+    it "can be created as a merchant_employee" do
+      user = User.create(name: "sammy",
+                         password: "pass",
+                         role: 1)
+      expect(user.role).to eq("merchant_employee")
+      expect(user.merchant_employee?).to be_truthy
+    end
+
+    it "can be created as a merchant_admin" do
+      user = User.create(name: "sammy",
+                         password: "pass",
+                         role: 2)
+      expect(user.role).to eq("merchant_admin")
+      expect(user.merchant_admin?).to be_truthy
+    end
+
+    it "can be created as an admin_user" do
+      user = User.create(name: "sammy",
+                         password: "pass",
+                         role: 3)
+      expect(user.role).to eq("admin_user")
+      expect(user.admin_user?).to be_truthy
+    end
+  end
 end
