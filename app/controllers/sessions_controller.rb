1b551231115b1358a990fec89f606a2a90f23ab7
Raymond Nguyen <rayknguyen@gmail.com>
2019-12-17 16:18:10 -0700

    Mod password confirmation
    Co-authored-by: Becky Robran <rerobran@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    if user != nil && user.authenticate(params[:password]) && user.authenticate(params[:password_confirmation])
+    if user != nil && user.authenticate(params[:password]) 
       session[:user_id] = user.id
       flash[:success] = "Welcome #{user.email}"
       redirect_to "/profile"
