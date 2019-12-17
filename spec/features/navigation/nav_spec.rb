6b6a566637f44ee174f9e30423c972c1a4e86aa7
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-16 18:13:21 -0700

    Add spec for visitor nav bar

---
+++ spec/features/navigation/nav_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'the navigation bar' do
+  it "shows these links for a visitor" do
+    visit "/"
+    click_link "Home"
+    expect(current_path).to eq "/"
+
+    visit "/"
+    click_link "Items"
+    expect(current_path).to eq "/items"
+
+    visit "/"
+    click_link "Merchants"
+    expect(current_path).to eq "/merchants"
+
+    visit "/"
+    click_link "Cart"
+    expect(current_path).to eq "/cart"
+
+    visit "/"
+    click_link "Login"
+    expect(current_path).to eq "/login"
+
+    visit "/"
+    click_link "Register"
+    expect(current_path).to eq "/register"
+  end
+end
