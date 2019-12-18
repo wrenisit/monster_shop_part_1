cd23bc351cd60c1f25b62df71f1e71fd3bb114da
Becky Robran <rerobran@gmail.com>
2019-12-18 14:28:18 -0700

    Adds test to most popular items index spec
    Co-Authored-By: Ray <itemniner@users.noreply.github.com>

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -61,7 +61,7 @@ RSpec.describe "Items Index Page" do
 
     it "can't see deactivated items" do
       visit '/items'
-      
+
       expect(page).to have_link(@tire.name)
       expect(page).to have_link(@tire.merchant.name)
       expect(page).to have_link(@pull_toy.name)
@@ -70,6 +70,43 @@ RSpec.describe "Items Index Page" do
       expect(page).to have_link(@dog_bone.merchant.name)
 
       expect(page).to_not have_content(@muzzle.name)
+    end
+
+    it "can list top 5 most popular items and quantity bought" do
+      visit '/items'
+
+        order_1 = create(:random_order)
+        order_2 = create(:random_order)
+
+        dog_bone = create(:random_item)
+        mug = create(:random_item)
+        boot = create(:random_item)
+        shirt = create(:random_item)
+        hat = create(:random_item)
+        cookie = create(:random_item)
+        pants = create(:random_item)
+
+        ItemOrder.create(item: dog_bone, order: order_1, price: dog_bone.price, quantity: 9)
+        ItemOrder.create(item: dog_bone, order: order_2, price: dog_bone.price, quantity: 2)
+        ItemOrder.create(item: dog_bone, order: order_2, price: dog_bone.price, quantity: 1)
+        ItemOrder.create(item: cookie, order: order_1, price: cookie.price, quantity: 7)
+        ItemOrder.create(item: cookie, order: order_2, price: cookie.price, quantity: 3)
+        ItemOrder.create(item: pants, order: order_2, price: pants.price, quantity: 8)
+        ItemOrder.create(item: mug, order: order_1, price: mug.price, quantity: 5)
+        ItemOrder.create(item: boot, order: order_1, price: boot.price, quantity: 4)
+        ItemOrder.create(item: shirt, order: order_2, price: shirt.price, quantity: 2)
+        ItemOrder.create(item: hat, order: order_2, price: hat.price, quantity: 1)
+
+
+
+require "pry"; binding.pry
+
+ItemOrder.select(:quantity)
+        # expect(page).to have_content(Item.most_popular)
+
+    end
+
+    xit "can list 5 least most popular items and quantity bought  " do
     end
   end
 end
