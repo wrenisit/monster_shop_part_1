5eeb9c3255b60c1bf0624e67e5db31fc873bf269
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-17 20:14:24 -0700

    Add method to check if merchant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart, :current_user
+  helper_method :cart, :current_user, :current_merchant_user?
 
   def current_user
     @user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_merchant_user?
+    current_user && (current_user.merchant_employee? || current_user.merchant_admin?)
   end
 
   def cart
