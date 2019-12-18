875d8223e6de84c330d033cece2d88aa412fb805
Becky Robran <rerobran@gmail.com>
2019-12-17 20:40:14 -0700

    Adds redirect functionality if you are already logged in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,32 @@
 class SessionsController < ApplicationController
 
   def new
+    if current_user != nil && current_user.user?
+      flash[:error] = "You are already logged in."
+      redirect_to "/profile"
+    elsif current_user != nil && current_user.merchant_admin?
+      flash[:error] = "You are already logged in."
+      redirect_to "/merchants/dashboard"
+    elsif current_user != nil && current_user.merchant_employee?
+      flash[:error] = "You are already logged in."
+      redirect_to "/merchants/dashboard"
+    elsif current_user != nil && current_user.admin_user?
+      flash[:error] = "You are already logged in."
+      redirect_to "/admin/dashboard"
+    end
   end
 
   def create
     user_login = User.find_by(email: params[:email])
-    if user_login != nil && user_login.authenticate(params[:password]) 
+    if user_login != nil && user_login.authenticate(params[:password])
       session[:user_id] = user_login.id
-      if user_login.user?  
+      if user_login.user?
         flash[:success] = "Welcome #{user_login.email}"
         redirect_to "/profile"
       elsif user_login.merchant_employee? || user_login.merchant_admin?
         flash[:success] = "Welcome Merchant #{user_login.email}"
         redirect_to "/merchants/dashboard"
-      elsif user_login.admin_user? 
+      elsif user_login.admin_user?
         flash[:success] = "Welcome Admin #{user_login.email}!"
         redirect_to "/admin/dashboard"
       end
