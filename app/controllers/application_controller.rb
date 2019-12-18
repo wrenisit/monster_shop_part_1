fd46230ee6681b51987448cb036862d01f995e8b
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-17 21:40:58 -0700

    Add current admin user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart, :current_user, :current_merchant_user?
+  helper_method :cart, :current_user, :current_merchant_user?, :current_admin_user?
+
+  def cart
+    session[:cart] ||= Hash.new(0)
+    @cart ||= Cart.new(session[:cart])
+  end
 
   def current_user
     @user ||= User.find(session[:user_id]) if session[:user_id]
@@ -11,9 +16,7 @@ class ApplicationController < ActionController::Base
     current_user && (current_user.merchant_employee? || current_user.merchant_admin?)
   end
 
-  def cart
-    session[:cart] ||= Hash.new(0)
-    @cart ||= Cart.new(session[:cart])
+  def current_admin_user?
+    current_user && current_user.admin_user?
   end
-
 end
