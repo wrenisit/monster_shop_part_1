f73aa18d66dda150f2c4988afc1060dac9ebe288
Becky Robran <rerobran@gmail.com>
2019-12-17 20:43:46 -0700

    Gets rid of commented out code

--- spec/features/users/profile_edit_spec.rb
+++ spec/features/users/profile_edit_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe "edit user info" do
 
     fill_in :email, with: "go@foogle.com"
     fill_in :password, with: "notsecure123"
-    fill_in :password_confirmation, with: "notsecure123"
+
     click_button "Log In"
 
     expect(current_path).to eq("/profile")
@@ -49,7 +49,7 @@ RSpec.describe "edit user info" do
 
     fill_in :email, with: this_regular_user.email
     fill_in :password, with: this_regular_user.password
-    fill_in :password_confirmation, with: this_regular_user.password
+
     click_button "Log In"
 
     expect(page).to have_content(this_regular_user.name)
@@ -60,15 +60,3 @@ RSpec.describe "edit user info" do
     expect(page).to have_content(this_regular_user.email)
   end
 end
-
-# As a registered user
-# When I visit my profile page
-# I see a link to edit my profile data
-# When I click on the link to edit my profile data
-# I see a form like the registration page
-# The form is prepopulated with all my current information except my password
-# When I change any or all of that information
-# And I submit the form
-# Then I am returned to my profile page
-# And I see a flash message telling me that my data is updated
-# And I see my updated information
