8efc580ca02405ee144a76e1edcb63d870e23a21
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-18 17:58:18 -0700

    Update spec to check status

--- spec/features/navigation/nav_restrictions_spec.rb
+++ spec/features/navigation/nav_restrictions_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 describe "user navigation" do
   it "restricts access to visitors" do
     visit "/merchant"
-    expect(page).to have_content "The page you were looking for doesn't exist."
+    expect(page.status_code).to eq(404)
 
     visit "/admin"
-    expect(page).to have_content "The page you were looking for doesn't exist."
+    expect(page.status_code).to eq(404)
 
     visit "/profile"
-    expect(page).to have_content "The page you were looking for doesn't exist."
+    expect(page.status_code).to eq(404)
   end
 end
