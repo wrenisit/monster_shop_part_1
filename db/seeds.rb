ade102b74afdef4f897898d585cc96860f2d2ef5
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-19 07:48:05 -0700

    Update seeds for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+include FactoryBot::Syntax::Methods
 
 Merchant.destroy_all
 Item.destroy_all
+Review.destroy_all
 
-#merchants
-bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+#creates 4 merchants
+jomah = create(:jomah_merchant)
+wren = create(:wren_merchant)
+becky = create(:becky_merchant)
+ray = create(:ray_merchant)
 
-#bike_shop items
-tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+# creates one item belonging to jomah and makes 10 reviews for it
+item_1 = create(:random_item, merchant: jomah)
+reviews = create_list(:random_review, 10, item: item_1)
 
-#dog_shop items
-pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
-dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+# creates 9 additional items for jomah; 6 active, 3 inactive
+jomah_items = create_list(:random_item, 6, merchant: jomah)
+hidden_jomah_items = create_list(:random_item, 3, merchant: jomah, active?: false)
+
+#creates 10 items for wren; 7 active, 3 inactive
+wren_items = create_list(:random_item, 7, merchant: wren)
+hidden_wren_items = create_list(:random_item, 3, merchant: wren, active?: false)
+
+#creates 10 items for becky; 7 active, 3 inactive
+beck_items = create_list(:random_item, 7, merchant: becky)
+hidden_becky_items = create_list(:random_item, 3, merchant: becky, active?: false)
+
+#creates 10 items for ray; 7 active, 3 inactive
+ray_items = create_list(:random_item, 7, merchant: ray)
+hidden_ray_items = create_list(:random_item, 3, merchant: ray, active?: false)
