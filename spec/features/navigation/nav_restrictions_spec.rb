c6092b4c25dcda41ed1b9119c4595602361fe8f0
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-18 18:39:36 -0700

    Add spec for user restrictions

--- spec/features/navigation/nav_restrictions_spec.rb
+++ spec/features/navigation/nav_restrictions_spec.rb
@@ -11,4 +11,15 @@ describe "user navigation" do
     visit "/profile"
     expect(page.status_code).to eq(404)
   end
+
+  it "restricts users from accessing /merchant and /admin" do
+    user = create(:regular_user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/merchant"
+    expect(page.status_code).to eq(404)
+
+    visit "/admin"
+    expect(page.status_code).to eq(404)
+  end
 end
