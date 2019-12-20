e70f8b4097ce60e524b6831044ff2aacc47448ac
Wren Steitle <lauren.steitle@gmail.com>
2019-12-20 09:41:08 -0700

    Update schema to use enums for order status

--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 20191220023615) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.string "status", default: "pending"
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
