3fab9c64ef74f3e1686a871c12dcb9b419fa96c3
Wren Steitle <lauren.steitle@gmail.com>
2019-12-20 09:43:35 -0700

    add enums to status

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,8 @@ class Order <ApplicationRecord
   has_many :item_orders, dependent: :destroy
   has_many :items, through: :item_orders, dependent: :destroy
   belongs_to :user
+  enum status: %w(pending packaged shipped cancelled)
+
 
   def grandtotal
     item_orders.sum('price * quantity')
