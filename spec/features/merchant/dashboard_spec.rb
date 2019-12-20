051ecfe00fb5071cc516be7ac600a67d8af14fa7
Becky Robran <rerobran@gmail.com>
2019-12-20 10:26:41 -0700

    Adds testing to merchant dashboard

--- spec/features/merchant/dashboard_spec.rb
+++ spec/features/merchant/dashboard_spec.rb
@@ -13,4 +13,9 @@ describe "merchant dashboard" do
     expect(page).to have_content merchant_employee.merchant.state
     expect(page).to have_content merchant_employee.merchant.zip
   end
+  it "if a user has pending orders containing items I sell I see a list of those orders" do
+    order
+  end
+  it "the order list includes order_id which is link to order show page, date created, total quantity of my items and total value for that order " do
+  end
 end
