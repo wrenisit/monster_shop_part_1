1091e6bf3f2e5b6de70bf45df38076452d41fde8
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-20 01:09:58 -0700

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ include FactoryBot::Syntax::Methods
 Merchant.destroy_all
 Item.destroy_all
 Review.destroy_all
+User.destroy_all
 
 #creates 4 merchants
 jomah = create(:jomah_merchant)
@@ -23,9 +24,31 @@ wren_items = create_list(:random_item, 7, merchant: wren)
 hidden_wren_items = create_list(:random_item, 3, merchant: wren, active?: false)
 
 #creates 10 items for becky; 7 active, 3 inactive
-beck_items = create_list(:random_item, 7, merchant: becky)
+becky_items = create_list(:random_item, 7, merchant: becky)
 hidden_becky_items = create_list(:random_item, 3, merchant: becky, active?: false)
 
 #creates 10 items for ray; 7 active, 3 inactive
 ray_items = create_list(:random_item, 7, merchant: ray)
 hidden_ray_items = create_list(:random_item, 3, merchant: ray, active?: false)
+
+#you can now log in as any of these users as defined in the factory
+user = create(:regular_user)
+create(:merchant_employee)
+create(:merchant_admin)
+create(:admin_user)
+
+#creates 3 orders each ordering a random quantity of each item
+order_1 = create(:random_order, user: user)
+Item.all.each do |item|
+  create(:item_order, order: order_1, item: item, price: item.price)
+end
+
+order_2 = create(:random_order, user: user)
+Item.all.each do |item|
+  create(:item_order, order: order_2, item: item, price: item.price)
+end
+
+order_3 = create(:random_order, user: user)
+Item.all.each do |item|
+  create(:item_order, order: order_3, item: item, price: item.price)
+end
