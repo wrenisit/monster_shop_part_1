e6f6581f833e52cd92bafc9f8ebe383a7a6d8b74
Becky Robran <rerobran@gmail.com>
2019-12-20 07:02:39 -0700

    Adds new action to cart controller to add/subtract items from cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -4,7 +4,6 @@ class CartController < ApplicationController
   def add_item
     item = Item.find(params[:item_id])
     cart.add_item(item.id.to_s)
-
     flash[:success] = "#{item.name} was successfully added to your cart"
     redirect_to "/items"
   end
@@ -23,13 +22,13 @@ class CartController < ApplicationController
     redirect_to '/cart'
   end
 
-  # def increment_decrement
-  #   if params[:increment_decrement] == "increment"
-  #     cart.add_quantity(params[:item_id]) unless cart.limit_reached?(params[:item_id])
-  #   elsif params[:increment_decrement] == "decrement"
-  #     cart.subtract_quantity(params[:item_id])
-  #     return remove_item if cart.quantity_zero?(params[:item_id])
-  #   end
-  #   redirect_to "/cart"
-  # end
+  def add_subtract_cart
+    if params[:add_subtract] == "add"
+      cart.add_quantity(params[:item_id]) unless cart.limit_reached?(params[:item_id])
+    elsif params[:add_subtract] == "subtract"
+      cart.subtract_quantity(params[:item_id])
+      return remove_item if cart.quantity_zero?(params[:item_id])
+    end
+    redirect_to "/cart"
+  end
 end
