337b1faab316ca48c58c89416aae52eb6e965061
Wren Steitle <lauren.steitle@gmail.com>
2019-12-19 19:33:46 -0700

    Update test to better test orders

--- spec/features/users/user_orders_index_spec.rb
+++ spec/features/users/user_orders_index_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "profile orders index page" do
-  xit "has a index of orders for user" do
+  it "has a index of orders for user" do
     @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
     @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-    regular_user = User.create(name: "Boo",
+    @regular_user = User.create(name: "Boo",
                                address: "123 Main",
                                city: "Broomfield",
                                state: "CO",
@@ -21,36 +21,20 @@ RSpec.describe "profile orders index page" do
      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
      @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
-     visit "/items/#{@paper.id}"
-     click_on "Add To Cart"
-     visit "/items/#{@tire.id}"
-     click_on "Add To Cart"
-     visit "/items/#{@pencil.id}"
-     click_on "Add To Cart"
-
-     visit "/cart"
-     expect(page).to have_link("Checkout")
-     click_on "Checkout"
-     expect(current_path).to eq("/orders/new")
-
-     fill_in :name, with: regular_user.name
-     fill_in :name, with: regular_user.address
-     fill_in :name, with: regular_user.city
-     fill_in :name, with: regular_user.state
-     fill_in :name, with: regular_user.zip
-     click_button "Create Order"
-     order = Order.last
+     @order = @regular_user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+     @order.item_orders.create!(item: @paper, price: @paper.price, quantity: 2)
+     @order.item_orders.create!(item: @pencil, price: @pencil.price, quantity: 3)
      visit "/profile"
      click_link "My Orders"
      expect(current_path).to eq("/profile/orders")
 
-     within "#order-#{order.id}"
-     expect(page).to have_link("#{order.id}")
-     expect(page).to have_content(order.created_at)
-     expect(page).to have_content(order.last_updated)
-     expect(page).to have_content(order.count)
-     expect(page).to have_content(order.total)
-     expect(page).to have_content(order.status)
+     within "#order-#{@order.id}"
+     expect(page).to have_link("#{@order.id}")
+     expect(page).to have_content(@order.created_at)
+     expect(page).to have_content(@order.updated_at)
+     expect(page).to have_content(@order.count)
+     expect(page).to have_content(@order.total)
+     expect(page).to have_content(@order.status)
   end
 end
 
