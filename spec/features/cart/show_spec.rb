6dcfd80def74102a147b00a84b31933f35f0df93
Becky Robran <rerobran@gmail.com>
2019-12-20 07:04:34 -0700

    Adds feature testing to adding/subtracting items to cart

--- spec/features/cart/show_spec.rb
+++ spec/features/cart/show_spec.rb
@@ -71,36 +71,44 @@ RSpec.describe 'Cart show' do
       end
 
       it "I can see a button/link to increment count of items I want to purchase" do
-        mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
         meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
 
         tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-        paper = mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
-        pencil = mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
-        visit "/items/#{paper.id}"
-        click_on "Add To Cart"
         visit "/items/#{tire.id}"
         click_on "Add To Cart"
-        visit "/items/#{pencil.id}"
-        click_on "Add To Cart"
-        items_in_cart = [paper, tire, pencil]
 
         visit '/cart'
 
-        within "#item-#{paper.id}" do
+        within "#item-#{tire.id}" do
           expect(page).to have_button("Add Item")
           expect(page).to have_content("1")
 
           click_on "Add Item"
           expect(page).to have_content("2")
         end
+          expect(page).to have_content("You have add another #{tire.name} to your cart.")
+          expect(current_path).to eq("/cart")
       end
-      it "I cannot increment the count beyond the item's inventory size" do
-        # @items_in_cart = [@paper,@tire,@pencil]
+
+      xit "I cannot increment the count beyond the item's inventory size" do
+        meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+        tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+        visit "/items/#{tire.id}"
+        click_on "Add To Cart"
+
         visit '/cart'
 
-      end
+        within "#item-#{tire.id}" do
+          11.times { click_on "Add To Cart" }
+          expect(page).to have_content("12")
 
+          click_on "Add Item"
+          expect(page).to have_content("12")
+        end
+          expect(page).to have_content("You cannot add any more #{tire.name} to your cart.")
+          expect(current_path).to eq("/cart")
+      end
     end
   end
 end
