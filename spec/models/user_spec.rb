28b3059a3f299330a7bb5a84dbab120109117969
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-20 23:13:17 -0700

    Add enum validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "validations" do
-    it {should validate_presence_of(:name)}
-    it {should validate_presence_of(:address)}
-    it {should validate_presence_of(:city)}
-    it {should validate_presence_of(:state)}
-    it {should validate_presence_of(:zip)}
-    it {should validate_presence_of(:email)}
-    it {should validate_uniqueness_of(:email)}
-    it {should validate_presence_of(:password)}
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :zip }
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :password }
+    it { should define_enum_for(:role).with_values([:user, :merchant_employee, :merchant_admin, :admin_user]) }
   end
   describe "relationships" do
     it { should have_many :orders}
