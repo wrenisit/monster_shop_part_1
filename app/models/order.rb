83415050e9f350205a4fb5aa71e43c5dc1ed4f7a
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-20 23:49:19 -0700

    Remove dependent destroy on joins table

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order <ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip, numericality: { only_integer: true, message: "must be a number" }
 
   has_many :item_orders, dependent: :destroy
-  has_many :items, through: :item_orders, dependent: :destroy
+  has_many :items, through: :item_orders
   belongs_to :user
 
   enum status: %w(pending packaged shipped cancelled)
