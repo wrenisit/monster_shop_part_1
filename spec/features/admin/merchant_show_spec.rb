593da2241b1d9e0f97fed95e7a0682f4b1043fb3
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-20 17:28:03 -0700

    Fix spec name

--- spec/features/admin/merchant_show_spec.rb
+++ spec/features/admin/merchant_show_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 RSpec.describe "admin mechant show page" do
   it "sees everything that merchant would see" do
-     user = create(:admin_user) 
+     user = create(:admin_user)
      ray = create(:ray_merchant)
 
      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
      visit "/merchants"
-
      click_on "#{ray.name}"
      expect(current_path).to eq("/admin/merchants/#{ray.id}")
      expect(page).to have_content(ray.name)
