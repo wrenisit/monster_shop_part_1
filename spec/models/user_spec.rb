453dd8781bcd147ffb99c51032f307d18350e503
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-20 23:47:04 -0700

    Update formatting

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :state }
     it { should validate_presence_of :zip }
     it { should validate_presence_of :email }
-    it { should validate_uniqueness_of :email }
     it { should validate_presence_of :password }
+    it { should validate_uniqueness_of :email }
     it { should define_enum_for(:role).with_values([:user, :merchant_employee, :merchant_admin, :admin_user]) }
   end
   describe "relationships" do
