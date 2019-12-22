b53831e4f446568a610eb37ede21c4d649a9865b
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-12-22 15:43:51 -0700

    Add additional tests

--- spec/features/admin/order_dashboard_spec.rb
+++ spec/features/admin/order_dashboard_spec.rb
@@ -4,15 +4,34 @@ describe "admin order dashboard" do
   it "shows packaged orders and can ship them" do
     user_1 = create(:random_user)
     user_2 = create(:random_user)
+    user_3 = create(:random_user)
+    user_4 = create(:random_user)
     admin_user = create(:admin_user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user)
 
     merchant = create(:jomah_merchant)
     order_1 = create(:random_order, user: user_1, status: "pending")
     order_2 = create(:random_order, user: user_2, status: "packaged")
-    order_3 = create(:random_order, user: user_1, status: "shipped")
-    order_4 = create(:random_order, user: user_2, status: "cancelled")
+    order_3 = create(:random_order, user: user_3, status: "shipped")
+    order_4 = create(:random_order, user: user_4, status: "cancelled")
 
     visit "/admin"
+
+    within "#packaged-orders" do
+      expect(page).to have_content user_2.name
+      expect(page).not_to have_content user_1.name
+      expect(page).not_to have_content user_3.name
+      expect(page).not_to have_content user_4.name
+    end
+
+    within "#order-#{order_2.id}" do
+      click_button "Ship Order"
+      expect(page).to have_content "Order Shipped"
+    end
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_2)
+
+    visit "/profile/orders/#{order_2.id}"
+    expect(page).not_to have_button "Cancel Order"
   end
 end
