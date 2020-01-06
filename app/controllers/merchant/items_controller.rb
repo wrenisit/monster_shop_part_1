a12b9fb34bdc844e28b4847697115dbeb690e5f7
Becky Robran <rerobran@gmail.com>
2020-01-05 20:37:25 -0700

    Adds merchant user item destroy action

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -3,4 +3,12 @@ class Merchant::ItemsController < Merchant::BaseController
     @merchant = current_user.merchant
     @items = @merchant.items
   end
+
+  def destroy
+    item = Item.find(params[:id])
+    Review.where(item_id: item.id).destroy_all
+    item.destroy
+    flash[:success] = "Item Deleted"
+    redirect_to merchant_dash_items_path
+  end
 end
