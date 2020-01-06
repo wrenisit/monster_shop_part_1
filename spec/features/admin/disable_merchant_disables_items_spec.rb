e2bd6ddfaa680baf97b1344eaad23ec37e79fd1c
Wren Steitle <lauren.steitle@gmail.com>
2020-01-06 16:46:43 -0700

    Creating test for user story 39

---
+++ spec/features/admin/disable_merchant_disables_items_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+RSpec.describe "disabling merchant disables items" do
+  it "disables items also" do
+    customer = create(:regular_user)
+    user = create(:admin_user)
+    ray = create(:ray_merchant)
+    merchant = create(:jomah_merchant)
+    item1 = create(:random_item, merchant: ray)
+    item2 = create(:random_item, merchant: ray)
+    item3 = create(:random_item, merchant: ray)
+    item4 = create(:random_item, merchant: ray)
+    item5 = create(:random_item, merchant: merchant)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/admin/merchants"
+    within "#merchant-#{ray.id}" do
+      click_button "Disable"
+    end
+    click_on "Log Out"
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(customer)
+    visit "/items"
+    
+  end
+end
