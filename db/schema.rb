4265e03325fa63622704a3322048181961049f40
Raymond Nguyen <rayknguyen@gmail.com>
2020-01-06 16:29:14 -0700

    Add scheme update for user for story 53

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191223035445) do
+ActiveRecord::Schema.define(version: 20200106223955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,8 @@ ActiveRecord::Schema.define(version: 20191223035445) do
     t.string "password_digest"
     t.integer "role", default: 0
     t.bigint "merchant_id"
+    t.datetime "created_at", default: -> { "now()" }, null: false
+    t.datetime "updated_at", default: -> { "now()" }, null: false
     t.index ["merchant_id"], name: "index_users_on_merchant_id"
   end
 
