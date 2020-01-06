35bb4ca094bceed011a5c3e1f4b9f0490f8746c1
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-05 19:23:50 -0700

    Add fulfill action to route

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -5,8 +5,12 @@ class Merchant::OrdersController < Merchant::BaseController
     @item_orders = merchant.item_orders_from(@order)
   end
 
-  def update
-    order = Order.find(params[:id])
-    order.update(status: "packaged")
+  def fulfill
+    merchant = current_user.merchant
+    order = merchant.orders.find(params[:id])
+    item_order = order.item_orders.find(params[:item_order_id])
+    item_order.fulfill
+    order.package_if_fulfilled
+    redirect_to merchant_dash_order_path(order)
   end
 end
