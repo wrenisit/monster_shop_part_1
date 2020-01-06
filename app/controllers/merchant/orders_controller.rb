644c9ede0c9f451df9e7026215b13afbcae9b7e0
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-05 18:51:06 -0700

    Update show action

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -1,6 +1,8 @@
 class Merchant::OrdersController < Merchant::BaseController
   def show
-    @order = Order.find(params[:id])
+    merchant = current_user.merchant
+    @order = merchant.orders.find(params[:id])
+    @item_orders = merchant.item_orders_from(@order)
   end
 
   def update
