419c1cf5ca828f5c7ad3834ee90e06286db5c4b6
Becky Robran <rerobran@gmail.com>
2020-01-06 07:46:02 -0700

    Updates controller for form partials

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,24 @@
 class ReviewsController<ApplicationController
 
   def new
+    @review = Review.new
     @item = Item.find(params[:item_id])
   end
 
   def create
     if field_empty?
-      item = Item.find(params[:item_id])
+      @item = Item.find(params[:item_id])
       flash[:error] = "Please fill in all fields in order to create a review."
-      redirect_to "/items/#{item.id}/reviews/new"
+      redirect_to "/items/#{@item.id}/reviews/new"
     else
       @item = Item.find(params[:item_id])
-      review = @item.reviews.create(review_params)
-      if review.save
+      @review = @item.reviews.create(review_params)
+      if @review.save
         flash[:success] = "Review successfully created"
         redirect_to item_path(@item)
       else
         flash[:error] = "Rating must be between 1 and 5"
-        render :new
+        redirect_to i"/items/#{@item.id}/reviews/new"
       end
     end
   end
