842ceef0a8bb036e65666e1b20febd861dc0d72f
Wren Steitle <lauren.steitle@gmail.com>
2020-01-06 15:06:56 -0700

    Fixed functionality for merchant deactivate

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -2,4 +2,17 @@ class Admin::MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    if merchant.active == true
+      merchant.toggle(:active)
+      flash[:notice] = "Merchant has been deactivated"
+      redirect_to "/admin/merchants"
+    else
+      merchant.toggle(:active)
+      flash[:notice] = "Merchant has been activated"
+      redirect_to "/admin/merchants"
+    end
+  end
 end
