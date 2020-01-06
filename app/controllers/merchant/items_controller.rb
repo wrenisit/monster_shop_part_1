0947e4e5a9e24a7647144e19b5aeb1f3f2b77f54
Becky Robran <rerobran@gmail.com>
2020-01-06 15:53:18 -0700

    Adds new and create actions to merchant item controller

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -4,11 +4,33 @@ class Merchant::ItemsController < Merchant::BaseController
     @items = @merchant.items
   end
 
+  def new
+    @merchant = current_user.merchant
+    @item = Item.new
+  end
+
+  def create
+    @merchant = current_user.merchant
+    @item = @merchant.items.create(item_params)
+    if @item.save
+      redirect_to merchant_dash_items_path
+    else
+      flash[:error] = @item.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
   def destroy
     item = Item.find(params[:id])
     Review.where(item_id: item.id).destroy_all
     item.destroy
     flash[:success] = "Item Deleted"
     redirect_to merchant_dash_items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:description,:price,:inventory,:image)
   end
 end
