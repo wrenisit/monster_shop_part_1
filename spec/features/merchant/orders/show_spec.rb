3cb06ce35beb8ca3200a40ae365870627e5baee0
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-05 18:42:10 -0700

    Add spec for item show

--- spec/features/merchant/orders/show_spec.rb
+++ spec/features/merchant/orders/show_spec.rb
@@ -2,20 +2,21 @@ require 'rails_helper'
 
 describe "merchant order show page" do
   before :each do
-    @user = create(:regular_user)
-    @merchant_user = create(:merchant_employee)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_user)
-
     @jomah = create(:jomah_merchant)
     @ray = create(:ray_merchant)
-    @order = create(:random_order, user: @user)
+
+    @user = create(:regular_user)
+    @merchant_user = create(:merchant_employee, merchant: @jomah)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_user)
 
     @item_1 = create(:random_item, merchant: @jomah, inventory: 10)
-    create(:item_order, order: @order, item: @item_1, price: @item_1.price, quantity: 5)
     @item_2 = create(:random_item, merchant: @jomah, inventory: 5)
-    create(:item_order, order: @order, item: @item_2, price: @item_2.price, quantity: 6)
     @item_3 = create(:random_item, merchant: @ray, inventory: 10)
-    create(:item_order, order: @order, item: @item_3, price: @item_3.price, quantity: 5)
+
+    @order = create(:random_order, user: @user)
+    @item_order_1 = create(:item_order, order: @order, item: @item_1, price: @item_1.price, quantity: 5)
+    @item_order_2 = create(:item_order, order: @order, item: @item_2, price: @item_2.price, quantity: 6)
+    @item_order_3 = create(:item_order, order: @order, item: @item_3, price: @item_3.price, quantity: 5)
   end
 
   it 'shows the customers name and address' do
@@ -30,5 +31,23 @@ describe "merchant order show page" do
     end
   end
 
+  it 'only shows my items' do
+    visit "/merchant/orders/#{@order.id}"
 
+    within "#item-#{@item_1.id}" do
+      expect(page).to have_link @item_1.name
+      expect(page).to have_css "img[src*='#{@item_1.image}']"
+      expect(page).to have_content @item_1.price
+      expect(page).to have_content @item_order_1.quantity
+    end
+
+    within "#item-#{@item_2.id}" do
+      expect(page).to have_link @item_2.name
+      expect(page).to have_css "img[src*='#{@item_2.image}']"
+      expect(page).to have_content @item_2.price
+      expect(page).to have_content @item_order_2.quantity
+    end
+
+    expect(page).not_to have_link @item_3.name
+  end
 end
