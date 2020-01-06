5e9700761d50a820434c3d11fb986f0a8fb07380
Raymond Nguyen <rayknguyen@gmail.com>
2020-01-06 14:42:40 -0700

    Add method for story 42

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -2,4 +2,12 @@ class Merchant::ItemsController < Merchant::BaseController
   def index
       @merchant = current_user.merchant
   end
+
+  def toggle_active 
+    item = Item.find(params[:item_id])
+    item.toggle!(:active?)
+    redirect_to "/merchant/items"
+    flash[:success] = "Item is no longer for sale."
+  end   
+
 end
