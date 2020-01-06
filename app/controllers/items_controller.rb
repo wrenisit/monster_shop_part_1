0379e736add5c95ec7a871985f97846e59986c0e
Becky Robran <rerobran@gmail.com>
2020-01-06 16:28:05 -0700

    Deletes old functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,36 +13,36 @@ class ItemsController<ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def new
-    @item = Item.new
-    @merchant = Merchant.find(params[:merchant_id])
-  end
+  # def new
+  #   @item = Item.new
+  #   @merchant = Merchant.find(params[:merchant_id])
+  # end
+  #
+  # def create
+  #   @merchant = Merchant.find(params[:merchant_id])
+  #   @item = @merchant.items.create(item_params)
+  #   if @item.save
+  #     redirect_to merchant_items_path(@merchant)
+  #   else
+  #     flash[:error] = @item.errors.full_messages.to_sentence
+  #     render :new
+  #   end
+  # end
 
-  def create
-    @merchant = Merchant.find(params[:merchant_id])
-    @item = @merchant.items.create(item_params)
-    if @item.save
-      redirect_to merchant_items_path(@merchant)
-    else
-      flash[:error] = @item.errors.full_messages.to_sentence
-      render :new
-    end
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
-      redirect_to item_path(@item)
-    else
-      flash[:error] = @item.errors.full_messages.to_sentence
-      render :edit
-    end
-  end
+  # def edit
+  #   @item = Item.find(params[:id])
+  # end
+  #
+  # def update
+  #   @item = Item.find(params[:id])
+  #   @item.update(item_params)
+  #   if @item.save
+  #     redirect_to item_path(@item)
+  #   else
+  #     flash[:error] = @item.errors.full_messages.to_sentence
+  #     render :edit
+  #   end
+  # end
 
   def destroy
     item = Item.find(params[:id])
@@ -52,8 +52,8 @@ class ItemsController<ApplicationController
   end
 
 private
-
-  def item_params
-    params.require(:item).permit(:name,:description,:price,:inventory,:image)
-  end
+  # 
+  # def item_params
+  #   params.require(:item).permit(:name,:description,:price,:inventory,:image)
+  # end
 end
