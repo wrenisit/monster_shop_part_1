4002359b0fb709aa06c691e6d93a2e6491c04d76
Becky Robran <rerobran@gmail.com>
2020-01-06 13:13:16 -0700

    Partials updates for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,15 +15,16 @@ class ItemsController<ApplicationController
 
   def new
     @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.new
   end
 
   def create
     @merchant = Merchant.find(params[:merchant_id])
-    item = @merchant.items.create(item_params)
-    if item.save
+    @item = @merchant.items.create(item_params)
+    if @item.save
       redirect_to merchant_items_path(@merchant)
     else
-      flash[:error] = item.errors.full_messages.to_sentence
+      flash[:error] = @item.errors.full_messages.to_sentence
       render :new
     end
   end
@@ -53,6 +54,6 @@ class ItemsController<ApplicationController
 private
 
   def item_params
-    params.permit(:name,:description,:price,:inventory,:image)
+    params.require(:item).permit(:name,:description,:price,:inventory,:image)
   end
 end
