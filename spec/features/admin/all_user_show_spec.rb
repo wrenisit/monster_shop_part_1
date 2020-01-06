269aa2096c92a2355058ef0f8534a07b1457519c
Raymond Nguyen <rayknguyen@gmail.com>
2020-01-06 16:28:01 -0700

    Add test for story 53

---
+++ spec/features/admin/all_user_show_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+RSpec.describe "admin all user show page" do
+  it "sees all user in the system" do
+     user = create(:admin_user)
+     ray = create(:regular_user, name: "Ray Win")
+     jomah = create(:regular_user, email: "rad@gmail.com")
+     wren = create(:regular_user, email: "rad1@gmail.com")
+     becky = create(:regular_user, email: "rad2@gmail.com")
+
+     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/admin/users"
+    
+    
+    expect(page).to have_content(ray.name)
+    expect(page).to have_content(ray.role)
+    expect(page).to have_content(ray.created_at)
+    expect(page).to have_content(jomah.name)
+    expect(page).to have_content(jomah.role)
+    expect(page).to have_content(jomah.created_at)
+    expect(page).to have_content(wren.name)
+    expect(page).to have_content(wren.role)
+    expect(page).to have_content(wren.created_at)
+    expect(page).to have_content(becky.name)
+    expect(page).to have_content(becky.role)
+    expect(page).to have_content(becky.created_at)
+    expect(User.all.count).to eq(5)
+
+    expect(page).to have_link(ray.name)
+
+    click_on "#{ray.name}"
+
+    expect(current_path).to eq("/admin/users/#{ray.id}")
+  end
+end
