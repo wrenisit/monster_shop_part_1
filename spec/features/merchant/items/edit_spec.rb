475206f5f886463d81e3a39927fd2bff5fc4b79d
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-07 11:13:33 -0700

    Formatting updates

--- spec/features/merchant/items/edit_spec.rb
+++ spec/features/merchant/items/edit_spec.rb
@@ -1,19 +1,3 @@
-# As a merchant
-# When I visit my items page
-# And I click the edit button or link next to any item
-# Then I am taken to a form similar to the 'new item' form
-# The form is pre-populated with all of this item's information
-# I can change any information, but all of the rules for adding a new item still apply:
-#
-# name and description cannot be blank
-# price cannot be less than $0.00
-# inventory must be 0 or greater
-# When I submit the form
-# I am taken back to my items page
-# I see a flash message indicating my item is updated
-# I see the item's new information on the page, and it maintains its previous enabled/disabled state
-# If I left the image field blank, I see a placeholder image for the thumbnail
-
 require 'rails_helper'
 
 RSpec.describe "As a merchant", type: :feature do
