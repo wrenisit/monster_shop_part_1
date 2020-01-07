b35aab9ffa4635d77c399ffca19bb16528832884
Raymond Nguyen <rayknguyen@gmail.com>
2020-01-07 15:32:22 -0700

    Add controller for user admin story 58

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,4 +6,42 @@ class Admin::UsersController < Admin::BaseController
   def show
     @display_user = User.find(params[:id])
   end
+
+  def edit
+    @display_user = User.find(params[:user_id])
+  end
+
+  def update
+    @display_user = User.find(params[:user_id])
+    if params[:password]
+      password_update
+    elsif @display_user.update(user_params)
+      flash[:success] = "Your profile has been updated."
+      redirect_to "/admin/users/#{@display_user.id}"
+    else
+      flash[:error] = @display_user.errors.full_messages.to_sentence
+      redirect_to "/admin/users/#{@display_user.id}"
+    end
+  end
+
+  def edit_password
+    @display_user = User.find(params[:user_id])
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
+  end
+
+  def password_update
+    if params[:password] == params[:password_confirmation]
+      @display_user.update(user_params)
+      flash[:success] = "Password has been updated."
+      redirect_to "/admin/users/#{@display_user.id}"
+    else
+      flash[:error] = "Passwords entered do not match."
+      redirect_to "/admin/users/#{@display_user.id}/edit_password"
+    end
+  end
 end
