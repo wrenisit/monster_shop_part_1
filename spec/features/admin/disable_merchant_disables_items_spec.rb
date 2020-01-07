66d045c2108c73b6f8f339ff13949eb50552b6fb
Wren Steitle <lauren.steitle@gmail.com>
2020-01-06 17:34:12 -0700

    Add additional testing for when merchant is enabled

--- spec/features/admin/disable_merchant_disables_items_spec.rb
+++ spec/features/admin/disable_merchant_disables_items_spec.rb
@@ -25,5 +25,23 @@ RSpec.describe "disabling merchant disables items" do
     expect(page).not_to have_content(item3.name)
     expect(page).not_to have_content(item4.name)
     expect(page).to have_content(item5.name)
+
+    click_on "Log Out"
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/admin/merchants"
+    within "#merchant-#{ray.id}" do
+      click_button "Enable"
+    end
+    click_on "Log Out"
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(customer)
+    visit "/items"
+    expect(page).to have_content(item1.name)
+    expect(page).to have_content(item2.name)
+    expect(page).to have_content(item3.name)
+    expect(page).to have_content(item4.name)
+    expect(page).to have_content(item5.name)
   end
 end
