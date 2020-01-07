37c85be5c7db3c27b25d9880c9b76b8704e511b9
Becky Robran <rerobran@gmail.com>
2020-01-07 12:59:30 -0700

    Adds column to user for active status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200106223955) do
+ActiveRecord::Schema.define(version: 20200107174606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,12 +31,12 @@ ActiveRecord::Schema.define(version: 20200106223955) do
     t.string "name"
     t.string "description"
     t.integer "price"
-    t.string "image"
     t.boolean "active?", default: true
     t.integer "inventory"
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image", default: "https://amp.businessinsider.com/images/5cdee90d021b4c15350f0c03-1136-852.jpg"
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20200106223955) do
     t.bigint "merchant_id"
     t.datetime "created_at", default: -> { "now()" }, null: false
     t.datetime "updated_at", default: -> { "now()" }, null: false
+    t.boolean "active", default: true
     t.index ["merchant_id"], name: "index_users_on_merchant_id"
   end
 
