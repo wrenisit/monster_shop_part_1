fd481d07af0ee84683ea2ae6588d7cab8d5b1531
Wren Steitle <lauren.steitle@gmail.com>
2020-01-08 11:23:45 -0700

    Fix validation names for messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  validates_presence_of :receiver_id, :sender_id, :title, :message
+  validates_presence_of :merchant_id, :user_id, :title, :body
 
   belongs_to :user
   belongs_to :merchant
