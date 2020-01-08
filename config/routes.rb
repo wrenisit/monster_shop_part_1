4eeb527008b6bf9b1bf37c15bbf154ac7bbb5413
Wren Steitle <lauren.steitle@gmail.com>
2020-01-08 11:47:27 -0700

    Fix route for messages index

--- config/routes.rb
+++ config/routes.rb
@@ -57,4 +57,6 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
+
+  get "/profile/messages", to: "users#messages_index"
 end
