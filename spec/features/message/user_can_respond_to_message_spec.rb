4569904406ef242dc56b9ddaf984858b0df2eefc
Wren Steitle <lauren.steitle@gmail.com>
2020-01-08 16:16:35 -0700

    Test for reply message is passing to merchant show page

--- spec/features/message/user_can_respond_to_message_spec.rb
+++ spec/features/message/user_can_respond_to_message_spec.rb
@@ -3,15 +3,8 @@ require 'rails_helper'
 RSpec.describe "User replies" do
   it "allows a user to respond" do
     merchant = create(:jomah_merchant)
-    regular_user = User.create(name: "Becky",
-                               address: "123 Main",
-                               city: "Broomfield",
-                               state: "CO",
-                               zip: 80020,
-                               email: "goble@foogle.com",
-                               password: "notsecure123",
-                               role: 1,
-                               merchant_id: merchant)
+    merchant_user = create(:merchant_employee, merchant: merchant)
+
     wren = create(:regular_user, email: "rad1@gmail.com")
     message_1 = Message.create(merchant_id: merchant.id, user_id: wren.id, title: "Order Delayed", body: "Hello. Due to a shipping delay, your order will not ship until July 12th. Thank you for your patience.", sender_id: merchant.id)
     message_2 = Message.create(merchant_id: merchant.id, user_id: wren.id, title: "Order Shipped", body: "Hello. Your order has shipped.", sender_id: merchant.id)
@@ -44,7 +37,7 @@ RSpec.describe "User replies" do
     end
 
     click_on "Log Out"
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(regular_user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant_user)
 
     visit "/merchant"
     expect(page).to have_content("Messages: ")
