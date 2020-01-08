1aa66f2792783d17809e5b1859c8ef80ada126df
Wren Steitle <lauren.steitle@gmail.com>
2020-01-08 10:22:08 -0700

    Complete initial test for messaging service

--- spec/features/message/user_sees_message_spec.rb
+++ spec/features/message/user_sees_message_spec.rb
@@ -6,22 +6,30 @@ RSpec.describe "user sees message" do
     wren = create(:regular_user, email: "rad1@gmail.com")
     becky = create(:regular_user, email: "rad2@gmail.com")
     message_1 = Message.create(receiver_id: wren.id, sender_id: merchant.id, title: "Order Delayed", message: "Hello. Due to a shipping delay, your order will not ship until July 12th. Thank you for your patience.")
+    message_2 = Message.create(receiver_id: wren.id, sender_id: merchant.id, title: "Order Shipped", message: "Hello. Your order has shipped.")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(wren)
 
     visit "/profile"
-    expect(page).to have_content("Messages")
+    expect(page).to have_content("Messages: ")
+    expect(page).to have_link("2")
+    click_link("2")
+    expect(current_path).to eq("/profile/messages")
     within "#message-#{message_1.id}" do
       expect(page).to have_content(merchant.name)
       expect(page).to have_content(message_1.status)
       expect(page).to have_link(message_1.title)
     end
-    within "#message-#{message_1.id}" do
+    within "#message-#{message_2.id}" do
       expect(page).to have_content(merchant.name)
       expect(page).to have_content(message_2.status)
       expect(page).to have_link(message_2.title)
       click_link("#{message_2.title}")
     end
-    expect(current_path).to eq("/")
-
+    expect(current_path).to eq("/profile/messages/#{message_2.id}")
+    expect(page).to have_content(message_2.title)
+    expect(page).to have_content(message_2.date_created)
+    expect(page).to have_content("Sender: #{message_2.sender}")
+    expect(page).to have_content(message_2.description)
+    
   end
 end
