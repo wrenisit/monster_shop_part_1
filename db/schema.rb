f853427c52da11a656685daf115e064e58b53c43
Wren Steitle <lauren.steitle@gmail.com>
2020-01-08 14:38:48 -0700

    update schema for sender in messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200108201726) do
+ActiveRecord::Schema.define(version: 20200108213419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20200108201726) do
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "sender_id"
     t.index ["merchant_id"], name: "index_messages_on_merchant_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
