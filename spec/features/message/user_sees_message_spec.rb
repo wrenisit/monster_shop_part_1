e9bad242eebe88277707eefc348adb8ef339ac65
Wren Steitle <lauren.steitle@gmail.com>
2020-01-08 11:23:27 -0700

    Add message testing back in

--- spec/features/message/user_sees_message_spec.rb
+++ spec/features/message/user_sees_message_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe "user sees message" do
     merchant = create(:jomah_merchant)
     wren = create(:regular_user, email: "rad1@gmail.com")
     becky = create(:regular_user, email: "rad2@gmail.com")
-    #message_1 = Message.create(receiver_id: wren.id, sender_id: merchant.id, title: "Order Delayed", message: "Hello. Due to a shipping delay, your order will not ship until July 12th. Thank you for your patience.")
-    #message_2 = Message.create(receiver_id: wren.id, sender_id: merchant.id, title: "Order Shipped", message: "Hello. Your order has shipped.")
+    message_1 = Message.create(merchant_id: merchant.id, user_id: wren.id, title: "Order Delayed", body: "Hello. Due to a shipping delay, your order will not ship until July 12th. Thank you for your patience.")
+    message_2 = Message.create(merchant_id: merchant.id, user_id: wren.id, title: "Order Shipped", body: "Hello. Your order has shipped.")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(wren)
 
     visit "/profile"
@@ -28,7 +28,7 @@ RSpec.describe "user sees message" do
     expect(current_path).to eq("/profile/messages/#{message_2.id}")
     expect(page).to have_content(message_2.title)
     expect(page).to have_content(message_2.date_created)
-    expect(page).to have_content("Sender: #{message_2.sender}")
+    expect(page).to have_content("Sender: #{message_2.merchant.name}")
     expect(page).to have_content(message_2.description)
 
   end
