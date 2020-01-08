9f7707dd4c2a012cdf89eb1f6f736cdf554ca107
Wren Steitle <lauren.steitle@gmail.com>
2020-01-08 10:46:13 -0700

    fix migration and add timestamps

--- db/migrate/20200108174355_create_messages.rb
+++ db/migrate/20200108174355_create_messages.rb
@@ -1,14 +1,13 @@
 class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
-      t.string :receiver_id
-      t.string :references
-      t.string :sender_id
-      t.string :references
+      t.references :receiver_id, foreign_key: true
+      t.references :sender_id, foreign_key: true
       t.string :title
       t.string :string
       t.string :message
       t.string :text
+      t.timestamps
     end
   end
 end
