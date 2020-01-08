b8d08910e412c2afd48930e4d156a427677de796
Wren Steitle <lauren.steitle@gmail.com>
2020-01-08 16:01:56 -0700

    add employee to test

--- spec/features/message/user_can_respond_to_message_spec.rb
+++ spec/features/message/user_can_respond_to_message_spec.rb
@@ -3,6 +3,15 @@ require 'rails_helper'
 RSpec.describe "User replies" do
   it "allows a user to respond" do
     merchant = create(:jomah_merchant)
+    regular_user = User.create(name: "Becky",
+                               address: "123 Main",
+                               city: "Broomfield",
+                               state: "CO",
+                               zip: 80020,
+                               email: "goble@foogle.com",
+                               password: "notsecure123",
+                               role: 1,
+                               merchant_id: merchant)
     wren = create(:regular_user, email: "rad1@gmail.com")
     message_1 = Message.create(merchant_id: merchant.id, user_id: wren.id, title: "Order Delayed", body: "Hello. Due to a shipping delay, your order will not ship until July 12th. Thank you for your patience.", sender_id: merchant.id)
     message_2 = Message.create(merchant_id: merchant.id, user_id: wren.id, title: "Order Shipped", body: "Hello. Your order has shipped.", sender_id: merchant.id)
@@ -19,7 +28,7 @@ RSpec.describe "User replies" do
 
     fill_in :title, with: "Thanks!"
     fill_in :body, with: "I can't wait to get my goods!"
-    click_button "Send"
+    click_button "Submit"
 
     expect(current_path).to eq("/profile/messages")
     expect(page).to have_content("Your message has been sent.")
@@ -35,7 +44,7 @@ RSpec.describe "User replies" do
     end
 
     click_on "Log Out"
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(regular_user)
 
     visit "/merchant"
     expect(page).to have_content("Messages: ")
