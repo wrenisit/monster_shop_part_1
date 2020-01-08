285a1b4ac40a97da0b7342cc531b97c6ef8c2b6f
Becky Robran <rerobran@gmail.com>
2020-01-07 20:47:10 -0700

    gets rid of variable merchant in index action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,6 @@ class ItemsController<ApplicationController
       @merchant = Merchant.find(params[:merchant_id])
       @items = @merchant.items
     else
-      @merchant = current_user.merchant
       @items = Item.active_items
     end
   end
