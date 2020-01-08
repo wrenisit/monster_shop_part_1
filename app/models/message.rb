56bb384217229e793b7c64862cc4ddde71eff38f
Wren Steitle <lauren.steitle@gmail.com>
2020-01-08 14:39:21 -0700

    Update validations for sender id

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  validates_presence_of :merchant_id, :user_id, :title, :body
+  validates_presence_of :merchant_id, :user_id, :title, :body, :sender_id
 
   belongs_to :user
   belongs_to :merchant
