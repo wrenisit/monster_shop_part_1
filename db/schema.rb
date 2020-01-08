fa582135a00ea4ce1d6dccc9215975c57d5ea00b
Wren Steitle <lauren.steitle@gmail.com>
2020-01-08 13:24:08 -0700

    Add timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200108184831) do
+ActiveRecord::Schema.define(version: 20200108201726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 20200108184831) do
     t.string "title"
     t.text "body"
     t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["merchant_id"], name: "index_messages_on_merchant_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
