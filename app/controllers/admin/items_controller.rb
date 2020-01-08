e5d024896bd18f7f6b81bb642ff46f9ee9dacc40
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-01-08 11:44:05 -0700

    Add admin toggle item action

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,4 +3,15 @@ class Admin::ItemsController < Admin::BaseController
     @merchant = Merchant.find(params[:merchant_id])
     @items = @merchant.items
   end
+
+  def toggle_active
+    item = Item.find(params[:item_id])
+    item.toggle!(:active?)
+    redirect_to admin_dash_merchant_items_path(item.merchant)
+    if item.active?
+      flash[:success] = "#{item.name} is now avalible for sale."
+    else
+      flash[:success] = "#{item.name} is no longer for sale."
+    end
+  end
 end
