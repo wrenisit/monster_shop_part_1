3b8b87536a9a9f90989e211cf8f6e1eb7707b801
Becky Robran <rerobran@gmail.com>
2020-01-08 13:31:03 -0700

    Creates search controller

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,15 @@
+class SearchController < ApplicationController
+  def search
+    if params[:search].blank?
+      flash[:alert] = "Empty field!"
+      redirect_to root_path
+    else
+      @parameter = params[:search].downcase
+      @results = Item.all.where("lower(items.name) LIKE :search", search: @parameter)
+      redirect_to "/results/index"
+    end
+  end
+
+  def index
+  end
+end
