aeb31b0963a0d008115d7fd44c0b4068b0a9c472
Wren Steitle <lauren.steitle@gmail.com>
2020-01-08 11:52:05 -0700

    Update messages table in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200108180815) do
+ActiveRecord::Schema.define(version: 20200108184831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20200108180815) do
     t.bigint "user_id"
     t.string "title"
     t.text "body"
+    t.integer "status", default: 0
     t.index ["merchant_id"], name: "index_messages_on_merchant_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
