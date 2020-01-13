e31ea8018373908fbf9e7d53396b841e9b6b986e
Wren Steitle <lauren.steitle@gmail.com>
2020-01-13 14:49:10 -0700

    Complete coupon creation method

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -10,4 +10,17 @@ class Merchant::CouponsController < Merchant::BaseController
 
   def new
   end
+
+  def create
+    merchant = Merchant.find(current_user.merchant_id)
+    merchant.coupons.create(coupon_params)
+    redirect_to "/merchant/coupons"
+  end
+
+  private
+
+  def coupon_params
+    params.permit(:name, :amount)
+  end
+
 end
